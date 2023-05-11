2faa0fa0df0969ea9d0752f9a328e570fb014b17
seohwan <seohwan91@gmail.com>
2023-05-11 17:08:39 +0900

    Added scripts

---
+++ palloc_coloring/palloc_coloring.py
@@ -0,0 +1,22 @@
+import yaml
+import os
+from os import path
+
+
+def palloc_coloring():
+    with open('palloc_coloring.yaml') as f:
+        data = yaml.load(f, yaml.FullLoader)
+        lxc1_color = data['lxc1_color']
+        lxc2_color = data['lxc2_color']
+    
+    # lxc1
+    cmd = "echo "+ str(lxc1_color) + " > /sys/fs/cgroup/palloc/lxc.payload.linux1/palloc.bins"
+    os.system(cmd)
+    # lxc2, running bash
+    cmd = "echo "+ str(lxc2_color) + " > /sys/fs/cgroup/palloc/lxc.payload.linux2/palloc.bins"
+    os.system(cmd)
+    cmd = "echo "+ str(os.getppid()) + " > /sys/fs/cgroup/palloc/lxc.payload.linux2/tasks"
+    os.system(cmd)
+    
+if __name__ == '__main__':
+    palloc_coloring()
---
+++ palloc_setup/palloc_setup.py
@@ -0,0 +1,26 @@
+import yaml
+import os
+from os import path
+
+
+def palloc_setup():
+    with open('palloc_setup.yaml') as f:
+        data = yaml.load(f, yaml.FullLoader)
+        enable_palloc = data['enable_palloc']
+        palloc_mask = data['palloc_mask']
+    
+    if enable_palloc == 'yes':
+        cmd = 'echo ' + str(palloc_mask) +' > /sys/kernel/debug/palloc/palloc_mask'
+        os.system(cmd)
+        cmd = 'echo 1 > /sys/kernel/debug/palloc/debug_level'
+        os.system(cmd)
+        cmd = 'echo 32 > /sys/kernel/debug/palloc/alloc_balance'
+        os.system(cmd)
+        cmd = 'echo 1 > /sys/kernel/debug/palloc/use_palloc'
+        os.system(cmd)
+    else:
+        cmd = 'echo 0 > /sys/kernel/debug/palloc/use_palloc'
+        os.system(cmd)
+
+if __name__ == '__main__':
+    palloc_setup()
